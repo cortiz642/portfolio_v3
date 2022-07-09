9747a88eef2603338a9513ba6fc8636ea8e96ad5
cortiz642 <chris.o.ortiz93@gmail.com>
2022-07-09 02:19:52 -0500

    added about and portfolio not complete

--- app.js
+++ app.js
@@ -5,3 +5,35 @@ const logo = document.querySelectorAll("#logo path");
 for(let i = 0; i < logo.length; i++){
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+function clickMenu(){
+    var navbar = document.getElementById('navbar');
+    var responsiveName = 'responsive';
+    navbar.classList.toggle(responsiveName);
+}
+    $("portfolio").html().document.onreadystatechange
+
+// const header = document.querySelector('.loader-wrapper');
+// window.onscroll = function(){
+//     var top = window.scrollY;
+//     console.log(top);
+//     if(top >= 50){
+//         header.classList.add('active')
+//     }else{
+//         header.classList.remove('active');
+//     }
+// }
+// document.onreadystatechange = function() {
+//             if (document.readyState !== "complete") {
+//                 document.querySelector(
+//                   "body").style.visibility = "hidden";
+//                 // $(".loader-wrapper").document.style.visibility="visible";
+//                 document.querySelector(
+//                   ".loader-wrapper").style.visibility = "visible";
+//             } else {
+//                 document.querySelector(
+//                   ".loader-wrapper").style.display = "visible";
+//                 document.querySelector(
+//                   "body").style.visibility = "hidden";
+//             }
+//         };
